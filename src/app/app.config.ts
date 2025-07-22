77384de27111615052187896ebeb3da4c0192562
Silas <silas.michael.hofmann@mni.thm.de>
2025-07-22 23:20:14 +0200

    add database service

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,14 +1,22 @@
-import { ApplicationConfig } from "@angular/core";
+import {APP_INITIALIZER, ApplicationConfig} from "@angular/core";
 import {PreloadAllModules, provideRouter, RouteReuseStrategy, withPreloading} from "@angular/router";
 
 import { routes } from "./app.routes";
 import {IonicRouteStrategy} from "@ionic/angular";
 import {provideIonicAngular} from "@ionic/angular/standalone";
+import {DatabaseService, initDbFactory} from "./services/database.service";
 
 export const appConfig: ApplicationConfig = {
   providers: [
     {
       provide: RouteReuseStrategy, useClass: IonicRouteStrategy
+    },
+    DatabaseService,
+    {
+      provide: APP_INITIALIZER,
+      useFactory: initDbFactory,
+      deps: [DatabaseService],
+      multi: true,
     },
     provideIonicAngular(),
     provideRouter(routes, withPreloading(PreloadAllModules)),
