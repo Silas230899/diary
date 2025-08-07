bce657e3312f2d75bcc07642d265b301ab995e30
Silas2308 <silas.michael.hofmann@mni.thm.de>
2025-08-07 10:20:47 +0200

    add right entries

--- src/app/today/today.page.ts
+++ src/app/today/today.page.ts
@@ -5,25 +5,51 @@ import {
   IonButton,
   IonCard, IonCardContent, IonCardHeader, IonCardTitle,
   IonContent,
-  IonHeader,
+  IonHeader, IonIcon,
   IonTitle,
   IonToolbar,
   NavController
 } from '@ionic/angular/standalone';
 import {addIcons} from "ionicons";
 import {add, createOutline, pencil} from "ionicons/icons";
+import {Entry} from "../models/entry";
+import {DatabaseService} from "../services/database.service";
 
 @Component({
   selector: 'app-today',
   templateUrl: './today.page.html',
   styleUrls: ['./today.page.scss'],
   standalone: true,
-  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule, IonButton, IonCard, IonCardContent, IonCardHeader, IonCardTitle]
+  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule, IonButton, IonCard, IonCardContent, IonCardHeader, IonCardTitle, IonIcon]
 })
 export class TodayPage implements OnInit {
 
-  constructor(private navController: NavController) {
+  date: string
+  entries: Entry[] = []
+
+  constructor(private navController: NavController, private dbService: DatabaseService) {
     addIcons({ add, pencil, createOutline })
+
+    this.date = new Date().toISOString()
+
+    this.populateEntries(this.date)
+  }
+
+  async populateEntries(date: string) {
+    this.entries = []
+    this.dbService.database.select("SELECT * FROM entry WHERE strftime('%m-%d', date) = strftime('%m-%d', date($1))", [date]).then(async res => {
+      // @ts-ignore
+      for(const entry of res) {
+        const text = await this.dbService.decryptData(entry["text"], "silas")
+        const entryObject = new Entry(
+            entry["id"],
+            entry["date"],
+            entry["written"],
+            text)
+        console.log(entryObject)
+        this.entries.push(entryObject)
+      }
+    })
   }
 
   async navigate(dst: string) {
