6371ae7c7591e9fe54cb4f81a194d5dd80622d96
Silas2308 <silas.michael.hofmann@mni.thm.de>
2025-09-24 21:21:15 +0200

    added onboarding page

---
+++ src/app/onboarding/onboarding.page.ts
@@ -0,0 +1,65 @@
+import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import {IonButton, IonContent, IonHeader, IonInput, IonNote, IonTitle, IonToolbar} from '@ionic/angular/standalone';
+import {PasswordService} from "../services/password.service";
+import {Router} from "@angular/router";
+import {SynchronizationService} from "../services/synchronization.service";
+import {ImageDb} from "../models/image-db";
+import {DatabaseService} from "../services/database.service";
+
+@Component({
+  selector: 'app-onboarding',
+  templateUrl: './onboarding.page.html',
+  styleUrls: ['./onboarding.page.scss'],
+  standalone: true,
+  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule, IonInput, IonButton, IonNote]
+})
+export class OnboardingPage implements OnInit {
+  
+  password = ""
+  googleInitialized: boolean
+
+  constructor(private passwordService: PasswordService,
+              private router: Router,
+              private sync: SynchronizationService,
+              private dbService: DatabaseService) {
+    this.googleInitialized = this.sync.isGoogleInitialized()
+  }
+
+  ngOnInit() {
+  }
+  
+  async initializeGoogle() {
+    await this.sync.google()
+    
+    const allFiles = await this.sync.listDriveFiles()
+    let done = 0
+    for(let file of allFiles) {
+      if(file.name.endsWith(".webp")) {
+        const imageData = await this.sync.downloadImage(file.id)
+        const image = new ImageDb(file.name, imageData)
+        await this.dbService.addImage(image)
+        done++
+      } else if(file.name.startsWith("entry-")) {
+        const entry = await this.sync.downloadEntry(file.id)
+        entry.driveFileId = file.id
+        await this.dbService.insertEntry(entry)
+        done++
+      }
+      console.log(done + "/" + allFiles.length)
+    }
+    
+    this.googleInitialized = this.sync.isGoogleInitialized()
+  }
+  
+  async testupload() {
+    await this.sync.uploadFile()
+    await this.sync.listFiles()
+  }
+  
+  async setPassword() {
+    await this.passwordService.setPassword(this.password);
+    await this.router.navigate(['/home'], { replaceUrl: true });
+  }
+}
