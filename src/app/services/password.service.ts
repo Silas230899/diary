50637ffe1eb44713f7d2fb22edfe60ae995f9ea0
Silas2308 <silas.michael.hofmann@mni.thm.de>
2025-10-01 22:30:07 +0200

    changed onboarding process to that an own password can be used

--- src/app/services/password.service.ts
+++ src/app/services/password.service.ts
@@ -14,48 +14,11 @@ export class PasswordService {
   
   constructor(private crypto: CryptoService) {}
   
-  async initMasterKeyIfPossible() {
-    const saltExists = await this.saltExists()
-    if(saltExists) {
-      let password: string | null = null
-      /*
-      if(platform() === "android") {
-        /-*
-        await store("?0@5Ue2YbCx)BP:i)Pu#KzxyK)WE)h)nN0K7+k*)!627_QCzLLxM9Mj!%5)-~fHMevjawB#P,t%qDBRR", {
-          keyAlias: "password",
-          promptTitle: "Passwort autorisieren",
-          promptSubtitle: "",
-          promptNegativeButtonText: "Abbrechen"
-        })
-        *-/
-        try {
-          password = await retrieve("password")
-        }catch(err) {
-          console.log(err)
-        }
-        
-      } else {
-        console.log("enter password:")
-      }
-      */
-      if(password === null) {
-        console.log("set default pw")
-        password = "?0@5Ue2YbCx)BP:i)Pu#KzxyK)WE)h)nN0K7+k*)!627_QCzLLxM9Mj!%5)-~fHMevjawB#P,t%qDBRR"
-        //return false
-      }
-      
-      //const password = "?0@5Ue2YbCx)BP:i)Pu#KzxyK)WE)h)nN0K7+k*)!627_QCzLLxM9Mj!%5)-~fHMevjawB#P,t%qDBRR"
-      const salt = await this.readSalt()
-      await this.crypto.initMasterKey(password, salt)
-      return true
-    } else return false
-  }
-  
   async saltExists() {
     return await exists(this.saltFilename, {baseDir: BaseDirectory.AppData})
   }
   
-  private async createSalt() {
+  async createSalt() {
     const salt = crypto.getRandomValues(new Uint8Array(16))
     await this.writeSalt(salt)
     return salt
