eee47196d7c3b403c75bb2e76f50ee0aac00133e
Silas2308 <silas.michael.hofmann@mni.thm.de>
2025-10-03 00:24:51 +0200

    date printing auslagern

---
+++ src/app/utils/dateStuff.ts
@@ -0,0 +1,48 @@
+function isSameDay(date1: Date, date2: Date) {
+  return (
+    date1.getUTCDate() === date2.getUTCDate() &&
+    date1.getMonth() === date2.getMonth() &&
+    date1.getFullYear() === date2.getFullYear()
+  );
+}
+
+export function formatDatetime(date: string, written: string | null, writtenHasTime: boolean | null) {
+  if(written !== null) {
+    if(isSameDay(new Date(date), new Date(written))) {
+      if(writtenHasTime!) {
+        const timeOptions: Intl.DateTimeFormatOptions = {
+          hour: "2-digit",
+          minute: "2-digit",
+        }
+        return "Heute (" + new Date(written).toLocaleTimeString(undefined, timeOptions) + " Uhr)"
+      } else {
+        return "Heute (unbekannte Uhrzeit)"
+      }
+    } else {
+      if(writtenHasTime!) {
+        const dateOptions: Intl.DateTimeFormatOptions = {
+          day: "2-digit",
+          month: "long",
+          year: "numeric",
+        }
+        const timeOptions: Intl.DateTimeFormatOptions = {
+          hour: "2-digit",
+          minute: "2-digit",
+        }
+        const dateString = new Date(written).toLocaleDateString(undefined, dateOptions)
+        const timeString = new Date(written).toLocaleTimeString(undefined, timeOptions)
+        
+        return `${dateString} (${timeString} Uhr)`
+      } else {
+        const options: Intl.DateTimeFormatOptions = {
+          day: "2-digit",
+          month: "short",
+          year: "numeric",
+        }
+        return new Date(written).toLocaleDateString(undefined, options) + " (unbekannte Uhrzeit)"
+      }
+    }
+  } else {
+    return "Unbekannt"
+  }
+}
