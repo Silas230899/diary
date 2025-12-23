a96d0aad09f84eb26be1d65aad5725847ebc323d
Silas <silas.michael.hofmann@mni.thm.de>
2025-12-23 18:14:17 +0100

    password has 10min expiration

--- src/app/guards/onboarding-guard.ts
+++ src/app/guards/onboarding-guard.ts
@@ -9,16 +9,23 @@ export const onboardingGuard: CanActivateFn = async (route, state) => {
   const crypto = inject(CryptoService);
   if(crypto.isMasterKeyInitialized()) return true
   else if(await passwordService.saltExists()) {
-    const password = localStorage.getItem("password")
-    if(password !== null) {
-      const salt = await passwordService.readSalt()
-      localStorage.setItem("password", password)
-      await crypto.initMasterKey(password, salt)
-      //await this.navCtrl.navigateRoot("home")
-      return true
-    }
-    // account is created, login with biometry or password
     const loginPath = router.parseUrl("/login")
+    const passwordExpirationDate = localStorage.getItem("passwordExpirationDate");
+    if(passwordExpirationDate !== null) {
+      console.log("PasswordExpirationDate: " + passwordExpirationDate);
+      if(new Date(passwordExpirationDate).getTime() > new Date().getTime()) {
+        const password = localStorage.getItem("password")
+        if(password !== null) {
+          const salt = await passwordService.readSalt()
+          //localStorage.setItem("password", password)
+          localStorage.setItem("passwordExpirationDate", new Date(new Date().getTime() + 10 * 60 * 1000).toISOString())
+          await crypto.initMasterKey(password, salt)
+          //await this.navCtrl.navigateRoot("home")
+          return true
+        }
+      } else localStorage.removeItem("password")
+    } else localStorage.removeItem("password")
+    // account is created, login with biometry or password
     return new RedirectCommand(loginPath)
   } else {
     // no account yet, create it first
